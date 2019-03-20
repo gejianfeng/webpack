02d833f158c6260dc488f5a706610ba174155aea
Ge Jian Feng <gejianfeng2@126.com>
2019-03-20 11:18:59 +0800

    + add webpack demo

---
+++ demo01/config/webpack.dev.js
@@ -0,0 +1,53 @@
+const path = require("path")
+const uglify = require("uglifyjs-webpack-plugin")
+const htmlPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "../src/main.js"),
+        main2: path.resolve(__dirname, "../src/main2.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 500
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new uglify(),
+        new htmlPlugin ({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: "./src/index.html"
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "../dist"),
+        host: "localhost",
+        compress: true,
+        port: 18080
+    }
+}
